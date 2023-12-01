fad81c79b22bcf874c60a8e7055c6e18497cb304
NTaylorRooof <nick.taylor@rooof.com>
2023-12-01 08:58:12 -0800

    performance improvements to day 1

--- days/day01.go
+++ days/day01.go
@@ -3,22 +3,29 @@ package days
 import (
 	"AoC_2023_Go/util"
 	"fmt"
-	"log"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
-var mp = map[string]string{
-	"one":   "1",
-	"two":   "2",
-	"three": "3",
-	"four":  "4",
-	"five":  "5",
-	"six":   "6",
-	"seven": "7",
-	"eight": "8",
-	"nine":  "9",
+var mp2 = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
 }
 
 func isDigit(c byte) bool {
@@ -34,46 +41,46 @@ func findDigits(line string) int {
 	for r >= 0 && !isDigit(line[r]) {
 		r--
 	}
-	number, _ := strconv.Atoi(string(line[l]) + string(line[r]))
+	number := (mp2[string(line[l])] * 10) + mp2[string(line[r])]
 	return number
 }
 
-func getCharOrWord(c byte, s string) string {
+func getCharOrWord(c byte, s string) int {
 	if isDigit(c) {
-		return string(c)
+		return mp2[string(c)]
 	} else if s != "" {
-		return mp[s]
+		return mp2[s]
 	}
-	return ""
+	return 0
 }
 
-func findWords(line string) int {
-	pattern, err := regexp.Compile(`(one|two|three|four|five|six|seven|eight|nine)`)
-	if err != nil {
-		log.Fatal("Bad Regex...")
-	}
-
+func findWords(line string, p regexp.Regexp) int {
 	var prefix strings.Builder
 	suffix := ""
-	n := ""
 
-	for i := 0; i < len(line); i++ {
-		prefix.WriteByte(line[i])
-		n += getCharOrWord(line[i], pattern.FindString(prefix.String()))
-		if n != "" {
+	l := 0
+	r := len(line) - 1
+
+	number := 0
+	for l < len(line) {
+		prefix.WriteByte(line[l])
+		number += getCharOrWord(line[l], p.FindString(prefix.String())) * 10
+		if number != 0 {
 			break
 		}
+		l++
 	}
 
-	for i := len(line) - 1; i >= 0; i-- {
-		suffix = string(line[i]) + suffix
-		n += getCharOrWord(line[i], pattern.FindString(suffix))
-		if len(n) == 2 {
+	for r >= 0 {
+		suffix = string(line[r]) + suffix
+		secondDigit := getCharOrWord(line[r], p.FindString(suffix))
+		if secondDigit != 0 {
+			number += secondDigit
 			break
 		}
+		r--
 	}
 
-	number, _ := strconv.Atoi(n)
 	return number
 }
 
@@ -82,9 +89,10 @@ func Day01() {
 	input := util.ReadInput(path)
 	p1_val := 0
 	p2_val := 0
+	pattern, _ := regexp.Compile(`(one|two|three|four|five|six|seven|eight|nine)`)
 	for _, line := range input {
 		p1_val += findDigits(line)
-		p2_val += findWords(line)
+		p2_val += findWords(line, *pattern)
 	}
 
 	fmt.Printf("Day 1:\n\tPart 1: The sum of the calibration values is %d.\n", p1_val)
