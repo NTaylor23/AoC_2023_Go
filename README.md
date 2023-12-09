42270604d5911fef2e8f581727231501cb7d22fb
NTaylor23 <c0502986@online.camosun.ca>
2023-12-09 12:06:18 -0800

    fix day 5

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 |[Cube Conundrum](https://adventofcode.com/2023/day/2)|[day02.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day02.go)|✅|✅|0.34|
 |[Gear Ratios](https://adventofcode.com/2023/day/3)|[day03.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day03.go)|✅|✅|1.36|
 |[Scratchcards](https://adventofcode.com/2023/day/4)|[day04.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day04.go)|✅|✅|0.74|
-|[If You Give A Seed A Fertilizer](https://adventofcode.com/2023/day/5)|[day05.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day05.go)|✅|🙀|Hmm|
+|[If You Give A Seed A Fertilizer](https://adventofcode.com/2023/day/5)|[day05.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day05.go)|✅|✅|0.79|
 |[Wait For It](https://adventofcode.com/2023/day/6)|[day06.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day06.go)|✅|✅|0.53|
 |[Camel Cards](https://adventofcode.com/2023/day/7)|[day07.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day07.go)|✅|✅|1.90|
 |[Haunted Wasteland](https://adventofcode.com/2023/day/8)|[day08.go](https://github.com/NTaylor23/AoC_2023_Go/blob/main/days/day08.go)|✅|✅|5.38|
--- days/day05.go
+++ days/day05.go
@@ -44,27 +44,61 @@ func Day05() {
 	path := "./inputs/day05.txt"
 	input := append(util.ReadInput(path), "")
 
-	p1_val, p2_val := 0, 0
+	p1_val := 0
 
 	nPat := regexp.MustCompile(`(\d+)`)
 	seeds := util.AtoiIter(nPat.FindAllString(input[0], -1))
 	minLoc := int(math.Inf(1))
 
-	maps := parseMaps(input[3:], nPat)
+	maps, pairs := parseMaps(input[3:], nPat), make([][]int, 0)
+
+	for i := 0; i < len(seeds); i += 2 {
+		pairs = append(pairs, []int{seeds[i], seeds[i] + seeds[i+1]})
+	}
+
 	for _, seed := range seeds {
 		minLoc = min(minLoc, traverse(seed, maps))
 	}
 	p1_val = minLoc
-	minLoc = int(math.Inf(1))
-	for i := 0; i < len(seeds); i += 2 {
-		start, end := seeds[i], seeds[i] + seeds[i + 1]
-		fmt.Printf("start: %v\n", start)
-		for start <= end {
-			minLoc = min(minLoc, traverse(start, maps))
-			start++
+
+	for _, mp := range maps {
+		new := make([][]int, 0)
+		for len(pairs) > 0 {
+			start, end := pairs[len(pairs)-1][0], pairs[len(pairs)-1][1]
+			pairs = pairs[:len(pairs)-1]
+			flag := false
+
+			for _, m := range mp {
+				dest, source, rng := m[0], m[1], m[2]
+				overlap_start := max(start, source)
+				overlap_end := min(end, source+rng)
+
+				if overlap_start < overlap_end {
+					new = append(new, []int{overlap_start - source + dest, overlap_end - source + dest})
+
+					if overlap_start > start {
+						pairs = append(pairs, []int{start, overlap_start})
+					}
+
+					if end > overlap_end {
+						pairs = append(pairs, []int{overlap_end, end})
+					}
+
+					flag = true
+					break
+				}
+			}
+			if !flag {
+				new = append(new, []int{start, end})
+			}
 		}
+		pairs = new
 	}
-	p2_val = minLoc
+	p2_val := pairs[0][0]
+
+	for _, p := range pairs {
+		p2_val = min(p2_val, min(p[0], p[1]))
+	}
 
 	fmt.Printf("Day 5:\n\tPart 1: The lowest location number is %d.\n", p1_val)
 	fmt.Printf("\tPart 2: The lowest location number in any range is %d.\n", p2_val)
